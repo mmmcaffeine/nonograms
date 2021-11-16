7b0efa053a12b96096fa73315e75501da873445b
Mark Roberts <mroberts@geodesys.com>
2021-11-16 21:09:01 +0000

    Implemented GlueStrategy

---
+++ src/Nonograms/Engine.Tests/GlueStrategyTests.cs
@@ -0,0 +1,44 @@
+﻿namespace Dgt.Nonograms.Engine.Tests;
+
+public class GlueStrategyTests
+{
+    public static TheoryData<int[], bool?[], bool?[]> MatchingHintExecuteTheoryData => new()
+    {
+        // Glued on the far right, and with nothing else to worry about
+        { new[] { 3 }, new bool?[] { true, null, null, null, null }, new bool?[] { true, true, true, null, null } },
+
+        // Glued on the far left, and with nothing else to worry about
+        { new[] { 3 }, new bool?[] { null, null, null, null, true }, new bool?[] { null, null, true, true, true } },
+
+        // Glued on the far left, and we should preserve cells on the right
+        { new[] { 3 }, new bool?[] { true, null, null, false, null }, new bool?[] { true, true, true, false, null } },
+
+        // Glued on the far right and we should preserve cells to the left
+        { new[] { 3 }, new bool?[] { false, false, null, null, true }, new bool?[] { false, false, true, true, true } },
+
+        // Glued on the left, but with some eliminated cells first
+        { new[] { 2 }, new bool?[] { false, false, true, null, null }, new bool?[] { false, false, true, true, null } },
+
+        // Glued on the right, but with some eliminated cells first, and cells on the left to preserve
+        { new[] { 2 }, new bool?[] { false, null, null, true, false }, new bool?[] { false, null, true, true, false } }
+    };
+
+    public static TheoryData<int[], bool?[]> NotMatchingHintExecuteTheoryData => new()
+    {
+        { new[] { 3 }, new bool?[] { null, null, null, null, null } },
+        { new[] { 3 }, new bool?[] { false, null, null, null, null } },
+        { new[] { 3 }, new bool?[] { false, null, null, null, false } },
+        { new[] { 3 }, new bool?[] { null, null, null, null, false } },
+        { new[] { 2 }, new bool?[] { false, null, true, false, null } }
+    };
+
+    [Theory]
+    [MemberData(nameof(MatchingHintExecuteTheoryData))]
+    public void Execute_Should_FillCellsAtEdgeOfGridWhenCellAtEdgeIsFilled(int[] hint, bool?[] cells, bool?[] expected) =>
+        new GlueStrategy().Execute(hint, cells).Should().Equal(expected);
+
+    [Theory]
+    [MemberData(nameof(NotMatchingHintExecuteTheoryData))]
+    public void Execute_Should_ReturnUnalteredCellsWhenCellAtEdgeOfGridIsNotFilled(int[] hint, bool?[] cells) =>
+        new GlueStrategy().Execute(hint, cells).Should().Equal(cells);
+}
---
+++ src/Nonograms/Engine/GlueStrategy.cs
@@ -0,0 +1,59 @@
+﻿namespace Dgt.Nonograms.Engine;
+
+public class GlueStrategy : IStrategy
+{
+    private const int NotFound = -1;
+
+    public IEnumerable<bool?> Execute(IEnumerable<int> hint, IEnumerable<bool?> cells)
+    {
+        var hintArray = hint.ToArray();
+        var cellsArray = cells.ToArray();
+
+        var gluedLeft = ExecuteLeftGlue(hintArray[0], cellsArray);
+        var gluedRight  = ExecuteRightGlue(hintArray[^1], gluedLeft);
+
+        return gluedRight;
+    }
+
+    private static bool?[] ExecuteLeftGlue(int hint, bool?[] cells)
+    {
+        var glued = new bool?[cells.Length];
+        var gluedAt = Array.FindIndex(cells, cell => cell == true);
+
+        if (gluedAt == NotFound) return cells;
+        if (cells[0..gluedAt].Any(cell => cell is null)) return cells;
+
+        Array.Fill(glued, false, 0, gluedAt);
+        Array.Fill(glued, true, gluedAt, hint);
+
+        for(var i = gluedAt + hint; i <glued.Length; i++)
+        {
+            glued[i] = cells[i];
+        }
+
+        return glued;
+    }
+
+    private static bool?[] ExecuteRightGlue(int hint, bool?[] cells)
+    {
+        var glued = new bool?[cells.Length];
+        var gluedAt = Array.FindLastIndex(cells, cell => cell == true);
+
+        if (gluedAt == NotFound) return cells;
+        if (cells[gluedAt..].Any(cell => cell is null)) return cells;
+
+        for(var i = 0; i < glued.Length; i++)
+        {
+            if (i <= gluedAt - hint)
+            {
+                glued[i] = cells[i];
+            }
+            else
+            {
+                glued[i] = i <= gluedAt;
+            }
+        }
+
+        return glued;
+    }
+}
