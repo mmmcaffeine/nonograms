55a6df87458d4be1c7d8b9f9ff8016a9ee5120e0
Mark Roberts <mroberts@geodesys.com>
2021-11-16 22:25:10 +0000

    Implemented an abstract base class for IStrategy implementations with common validation logic

---
+++ src/Engine.Tests/StrategyBaseTests.cs
@@ -0,0 +1,92 @@
+﻿namespace Dgt.Nonograms.Engine.Tests;
+
+public class StrategyBaseTests
+{
+    private class TestableStrategy : StrategyBase
+    {
+        protected override bool?[] DoExecute(int[] hint, bool?[] cells)
+        {
+            return cells;
+        }
+    }
+
+    public static TheoryData<int, int[], bool?[]> InvalidHintTestData => new()
+    {
+        { 5, new[] { 5 }, new bool?[] { true, false, true, false } },
+        { 7, new[] { 3, 3 }, new bool?[] { false, false, false } },
+        { 6, new[] { 1, 2, 1 }, new bool?[] { null, null } }
+    };
+
+    [Fact]
+    public void Execute_Should_ThrowWhenHintIsNull()
+    {
+        // Arrange
+        var sut = new TestableStrategy();
+
+        // Act
+        var act = () => _ = sut.Execute(null!, new bool?[] { null });
+
+        // Assert
+        act.Should().Throw<ArgumentNullException>().WithParameterName("hint");
+    }
+
+    [Fact]
+    public void Execute_Should_ThrowWhenHintIsEmpty()
+    {
+        // Arrange
+        var sut = new TestableStrategy();
+
+        // Act
+        var act = () => _ = sut.Execute(Array.Empty<int>(), new bool?[] { null });
+
+        // Assert
+        act.Should().Throw<ArgumentException>()
+            .WithParameterName("hint")
+            .WithMessage("Value must contain at least one item.*");
+    }
+
+    [Fact]
+    public void Execute_Should_ThrowWhenCellsIsNull()
+    {
+        // Arrange
+        var sut = new TestableStrategy();
+
+        // Act
+        var act = () => _ = sut.Execute(new[] { 1 }, null!);
+
+        // Assert
+        act.Should().Throw<ArgumentNullException>().WithParameterName("cells");
+    }
+
+    [Fact]
+    public void Execute_Should_ThrowWhenCellsIsEmpty()
+    {
+        // Arrange
+        var sut = new TestableStrategy();
+
+        // Act
+        var act = () => _ = sut.Execute(new[] { 1 }, Array.Empty<bool?>());
+
+        // Assert
+        act.Should().Throw<ArgumentException>()
+            .WithParameterName("cells")
+            .WithMessage("Value must contain at least one item.*");
+    }
+
+    [Theory]
+    [MemberData(nameof(InvalidHintTestData))]
+    public void Execute_Should_Throw_WhenHintIsGreaterThanCells(int minimumCells, int[] hint, bool?[] cells)
+    {
+        // Arrange
+        var sut = new TestableStrategy();
+
+        // Act
+        var act = () => _ = sut.Execute(hint, cells);
+
+        // Assert
+        act.Should().Throw<ArgumentException>()
+            .WithParameterName("hint")
+            .WithMessage("It is not possible to fill in enough cells to fulfil the hint.*")
+            .WithMessage($"*The hint would require at least {minimumCells} cells, but there are only {cells.Length}.*");
+    }
+}
---
+++ src/Engine/StrategyBase.cs
@@ -0,0 +1,32 @@
+﻿namespace Dgt.Nonograms.Engine;
+
+public abstract class StrategyBase : IStrategy
+{
+    public IEnumerable<bool?> Execute(IEnumerable<int> hint, IEnumerable<bool?> cells)
+    {
+        var hintArray = hint is not null ? hint.ToArray() : throw new ArgumentNullException(nameof(hint));
+        var cellsArray = cells is not null ? cells.ToArray() : throw new ArgumentNullException(nameof (cells));
+
+        if (hintArray.Length == 0) throw new ArgumentException("Value must contain at least one item.", nameof(hint));
+        if (cellsArray.Length == 0) throw new ArgumentException("Value must contain at least one item.", nameof(cells));
+
+        var minimumCells = hintArray.Sum() + hintArray.Length - 1;
+
+        if (minimumCells > cellsArray.Length)
+        {
+            // You wouldn't normally concat strings like this as it is inefficient, but we're not exactly going to be hitting
+            // this code a lot so I don't care that much 😉
+
+            var message = "It is not possible to fill in enough cells to fulfil the hint.";
+            message += $" The hint would require at least {minimumCells} cells, but there are only {cellsArray.Length}.";
+
+            throw new ArgumentException(message, nameof(hint));
+        }
+
+        // TODO Check that what comes back from DoExecute does not conflict with the incoming cells
+
+        return DoExecute(hintArray, cellsArray);
+    }
+
+    protected abstract bool?[] DoExecute(int[] hint, bool?[] cells);
+}
