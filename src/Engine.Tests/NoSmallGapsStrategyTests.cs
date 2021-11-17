08ae802dde068f7363ad7f37ee3b8379d594f6f1
Mark Roberts <mroberts@geodesys.com>
2021-11-17 18:37:51 +0000

    Implemented NoSmallGapsStrategy
    This strategy finds gaps that are smaller than the smallest hint i.e that cannot possibly be the filled cells and eliminates them.

---
+++ src/Engine.Tests/NoSmallGapsStrategyTests.cs
@@ -0,0 +1,22 @@
+﻿namespace Dgt.Nonograms.Engine.Tests;
+
+public class NoSmallGapsStrategyTests
+{
+    public static TheoryData<int[], bool?[], bool?[]> TheoryData => new()
+    {
+        // Hint of 1 means we can't eliminate any gaps because any gap could be the filled cell
+        { new[] { 1 }, new bool?[] { false, null, false, null, null, false }, new bool?[] { false, null, false, null, null, false } },
+
+        // The 5 would allow us to eliminate some cells, but the 1 could go anywhere so we're still stuffed (but see the ideas.md file)
+        { new[] { 1, 5 }, new bool?[] { false, null, false, null, null, null, false, null }, new bool?[] { false, null, false, null, null, null, false, null } },
+
+        // We can eliminate the two gaps of two because the 3 cannot go there. In this case we know the 3 has to go
+        // on the far right, but that is not our responsibility!
+        { new[] { 3 }, new bool?[] { null, null, false, null, null, false, null, null, null }, new bool?[] { false, false, false, false, false, false, null, null, null } },
+    };
+
+    [Theory]
+    [MemberData(nameof(TheoryData))]
+    public void Execute_Should_EliminateAnyGapsSmallerThanSmallestHint(int[] hint, bool?[] cells, bool?[] expected) =>
+        new NoSmallGapsStrategy().Execute(hint, cells).Should().Equal(expected);
+}
---
+++ src/Engine/NoSmallGapsStrategy.cs
@@ -0,0 +1,67 @@
+﻿namespace Dgt.Nonograms.Engine;
+
+public class NoSmallGapsStrategy : StrategyBase
+{
+    // TODO You've got lots of copying of arrays and other enumerables (because you're using yield) which you should eliminate
+    // TODO You can special case when the smallest hint is one, but then you should have tests for a multi-part hint where
+    //      where the smaller number stops you eliminating other gaps
+    protected override bool?[] Execute(int[] hint, bool?[] cells)
+    {
+        var smallestHint = hint.Min();
+        var indicesOfEliminatedCells = FindAllIndices(cells, c => c == false).ToArray();
+        var gapsBetweenEliminatedCells = FindGaps(indicesOfEliminatedCells, cells.Length).ToArray();
+        var values = new bool?[cells.Length];
+
+        Array.Copy(cells, values, cells.Length);
+
+        foreach(var (index, length) in gapsBetweenEliminatedCells)
+        {
+            if(length >= smallestHint)
+            {
+                continue;
+            }
+
+            if (values[index..(index + length - 1)].Any(value => value is not null))
+            {
+                continue;
+            }
+
+            for(int i = 0; i <= length; i++)
+            {
+                values[index + i] = false;
+            }
+        }
+
+        return values;
+    }
+
+    private static IEnumerable<int> FindAllIndices(bool?[] values, Func<bool?, bool> predicate)
+    {
+        for (var i = 0; i < values.Length; i++)
+        {
+            if(predicate(values[i]))
+            {
+                yield return i;
+            }
+        }
+    }
+
+    // TODO Does it make sense to return a tuple of (Range Range, int Length)? That would make this data easier to consume when enumerating it
+    private static IEnumerable<(int Index, int Length)> FindGaps(int[] indices, int length)
+    {
+        if(indices[0] > 0)
+        {
+            yield return (0, indices[0]);
+        }
+
+        for(var i = 0; i < indices.Length - 1; i++)
+        {
+            yield return (indices[i] + 1, indices[i + 1] - indices[i] - 1);
+        }
+
+        if (indices[^1] < length - 1)
+        {
+            yield return (indices[^1] + 1, length - indices[^1] - 1);
+        }
+    }
+}
