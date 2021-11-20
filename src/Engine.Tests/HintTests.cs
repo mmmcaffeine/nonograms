b208b9cc4bce4efe24d6c43cc4b43448def2fceb
Mark Roberts <mroberts@geodesys.com>
2021-11-20 14:00:34 +0000

    Added Hint type with constructor validation logic

---
+++ src/Engine.Tests/HintTests.cs
@@ -0,0 +1,49 @@
+﻿namespace Dgt.Nonograms.Engine.Tests;
+
+public class HintTests
+{
+    [Fact]
+    public void Ctor_Should_ThrowWhenElementsIsNull()
+    {
+        // Arrange, Act
+        var act = () => _ = new Hint(null!);
+
+        // Assert
+        act.Should().Throw<ArgumentNullException>().WithParameterName("elements");
+    }
+
+    [Fact]
+    public void Ctor_Should_ThrowWhenElementsIsEmpty()
+    {
+        // Arrange, Act
+        var act = () => _ = new Hint(Array.Empty<uint>());
+
+        // Assert
+        act.Should().Throw<ArgumentException>()
+            .WithParameterName("elements")
+            .WithMessage("*Value must contain at least one item.*");
+    }
+
+    // xUnit can't resolve the parameters to the method properly if we type them as uint 😢 We have to type them as int, then convert
+    // ourselves to consume the constructor
+    [Theory]
+    [InlineData("zero at index 0", 0)]
+    [InlineData("zeroes at indices 0, and 1", 0, 0)]
+    [InlineData("zero at index 0", 0, 1)]
+    [InlineData("zero at index 1", 1, 0)]
+    [InlineData("zeroes at indices 0, 1, 3, and 4", 0, 0, 1, 0, 0)]
+    [InlineData("zero at index 2", 1, 1, 0, 1, 1)]
+    public void Ctor_Should_ThrowWhenElementsContainsZeros(string messageFragment, params int[] elements)
+    {
+        // Arrange, Act
+        var uintElements = elements.Cast<uint>();
+        var act = () => _ = new Hint(uintElements);
+
+        // Assert
+        act.Should().Throw<ArgumentException>()
+            .WithParameterName("elements")
+            .WithMessage("*Value cannot contain zeroes.*")
+            .WithMessage($"*Found {messageFragment}.*")
+            .Where(ex => ex.Data.Contains("elements") && ReferenceEquals(ex.Data["elements"], uintElements));
+    }
+}
---
+++ src/Engine/Hint.cs
@@ -0,0 +1,50 @@
+﻿using System.Text;
+
+namespace Dgt.Nonograms.Engine;
+
+public sealed class Hint
+{
+    private readonly uint[] _elements;
+
+    public Hint(IEnumerable<uint> elements)
+    {
+        if (elements is null) throw new ArgumentNullException(nameof(elements));
+
+        _elements = elements.ToArray();
+
+        if (!_elements.Any()) throw new ArgumentException("Value must contain at least one item.", nameof(elements));
+        if (_elements.Any(x => x == 0)) throw CreateContainsZeroesException(_elements, elements, nameof(elements));
+    }
+
+    private static Exception CreateContainsZeroesException(uint[] elements, IEnumerable<uint> actualValue, string paramName)
+    {
+        var messageBuilder = new StringBuilder("Value cannot contain zeroes.");
+        var zeroes = elements.Select((x, i) => (Element: x, Index: i))
+            .Where(t => t.Element == 0)
+            .ToList();
+
+        if(zeroes.Count == 1)
+        {
+            messageBuilder.Append($" Found zero at index {zeroes[0].Index}");
+        }
+        else
+        {
+            messageBuilder.Append(" Found zeroes at indices ");
+
+            for(var i = 0; i < zeroes.Count; i++)
+            {
+                if (i > 0) messageBuilder.Append(", ");
+                if (i == zeroes.Count - 1) messageBuilder.Append("and ");
+
+                messageBuilder.Append(zeroes[i].Index);
+            }
+        }
+
+        messageBuilder.Append('.');
+
+        return new ArgumentException(messageBuilder.ToString(), paramName)
+        {
+            Data = { { paramName, actualValue} }
+        };
+    }
+}
